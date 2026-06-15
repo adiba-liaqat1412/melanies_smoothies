e00c09b44a1b0de00f25092642a5e3cb8c349c07
adiba-liaqat1412 <AdibaLiaqat@cygnethealth.co.uk>
2026-06-15 11:38:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import request
 
 
 # Write directly to the app
@@ -44,3 +45,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
