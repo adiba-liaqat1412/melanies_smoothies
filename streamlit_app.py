680c9f503b67fb17fc36d6c390deb60727c9a404
adiba-liaqat1412 <AdibaLiaqat@cygnethealth.co.uk>
2026-06-15 13:07:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
   """
 )
 name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smooethie will be:', name_on_order)
+#st.write('The name on your Smooethie will be:', name_on_order)
 
 # option = st.selectbox('What is your favourite fruit?',
 # ('Banana','Strawberries','Peaches'))
@@ -37,7 +37,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         #st.stop()
       
