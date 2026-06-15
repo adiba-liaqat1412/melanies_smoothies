905b5dd98ddd2395b09a343c04ccdcb6a0e8d6db
adiba-liaqat1412 <AdibaLiaqat@cygnethealth.co.uk>
2026-06-15 13:04:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.stop()
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #st.stop()
       
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")  
@@ -49,8 +49,8 @@ if ingredients_list:
         
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients)
-                    values ('""" + name_on_order + """', '""" + ingredients_string + """')"""
+        my_insert_stmt = """ insert into smoothies.public.orders(name_on_order,ingredients)
+        values ('""" + name_on_order + """', '""" + ingredients_string + """')"""
     
 
     st.write(my_insert_stmt)
